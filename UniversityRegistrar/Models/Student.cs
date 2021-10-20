85c5fca93d58beacb3a1dd5c7839e5e0c12c6bba
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-10-19 20:46:29 -0700

    Add display names and required form fields
    Co-authored-by: William Sercombe <wsercombe@gmail.com>
    Co-authored-by: Shane Graff <copellius@gmail.com>

--- UniversityRegistrar/Models/Student.cs
+++ UniversityRegistrar/Models/Student.cs
@@ -1,7 +1,10 @@
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace UniversityRegistrar.Models
+
 {
   public class Student
   {
@@ -11,7 +14,11 @@ namespace UniversityRegistrar.Models
     }
 
     public int StudentId { get; set; }
+    [DisplayName("Student Name")]
+    [Required(ErrorMessage = "Student name is required")]
     public string StudentName { get; set; }
+    [DisplayName("Enrollment Date")]
+    [Required(ErrorMessage = "Enrollment date is required")]
     public DateTime EnrollmentDate {get; set;}
 
     public virtual ICollection<Registrar> JoinEntities { get;}
